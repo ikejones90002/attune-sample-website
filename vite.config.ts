86ca1ad75489ec18bd3c236f0b694f70e3bcd24c
Isaac Jones <103softwaresolutionsllc@gmail.com>
2026-08-15 13:36:47 -0700

    Revert base path for local development

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/attune-sample-website/",
   server: {
     host: true,
     allowedHosts: true,
